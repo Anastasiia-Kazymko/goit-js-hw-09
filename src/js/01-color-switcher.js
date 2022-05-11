51c09aa5a34d32554184aa2125e0bfa99fb58f65
Anastasiia8888 <92369437+Anastasiia8888@users.noreply.github.com>
2022-05-11 15:57:22 +0300

    push 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
+const COLOR_DELAY = 1000;
+let intervalId = null;
+const refs = {
+  body: document.body,
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', changeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
+
+function changeColor() {       
+        refs.startBtn.disabled = true;
+        intervalId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, COLOR_DELAY)    
+  };
+
+function stopChangeColor() {
+    clearInterval(intervalId);
+    refs.startBtn.disabled = false;    
+};
